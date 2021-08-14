94b7ae1ba86238f8424556bcf44204f869e2658b
Wisse <wissehes@gmail.com>
2021-08-14 22:52:43 +0200

    Initial Commit

---
+++ Loulou's eten/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Loulou's eten
+//
+//  Created by Wisse Hes on 14/08/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Loulou's eten/Loulou_s_etenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Loulou_s_etenApp.swift
+//  Loulou's eten
+//
+//  Created by Wisse Hes on 14/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct Loulou_s_etenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
