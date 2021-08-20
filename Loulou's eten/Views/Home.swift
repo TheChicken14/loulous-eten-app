cc97420b5715fbe94a5fccba8947cb1ff41ecfdc
Wisse <wissehes@gmail.com>
2021-08-20 20:38:37 +0200

    Put the Home view in a NavigationView and remove feedingHistory toolbar item

--- Loulou's eten/Views/Home.swift
+++ Loulou's eten/Views/Home.swift
@@ -12,109 +12,106 @@ struct Home: View {
     @StateObject var viewModel = HomeViewModel()
     
     var body: some View {
-        ZStack {
-            GradientBackground()
-            
-            if !viewModel.loading {
-                VStack {
-                    Text(LocalizedStringKey( viewModel.breakfastOrDinner))
-                        .font(.headline)
-                    
-                    Label(viewModel.hasFood ? "home.food.yes" : "home.food.no", systemImage: viewModel.hasFood ? "checkmark.circle.fill" : "xmark.circle.fill")
-                        .font(.system(size: 40, weight: .bold, design: .rounded))
-                        .padding(40)
-                    
-                    if viewModel.hasFood {
-                        if let date = viewModel.whenGotFood, let name = viewModel.feeder {
-                            Text("home.fedByOnTime \(date, style: .time) \(name)")
-                                .font(.title)
-                                .padding()
+        NavigationView {
+            ZStack {
+                GradientBackground()
+                
+                if !viewModel.loading {
+                    VStack {
+                        Text(LocalizedStringKey( viewModel.breakfastOrDinner))
+                            .font(.headline)
+                        
+                        Label(viewModel.hasFood ? "home.food.yes" : "home.food.no", systemImage: viewModel.hasFood ? "checkmark.circle.fill" : "xmark.circle.fill")
+                            .font(.system(size: 40, weight: .bold, design: .rounded))
+                            .padding(40)
+                        
+                        if viewModel.hasFood {
+                            if let date = viewModel.whenGotFood, let name = viewModel.feeder {
+                                Text("home.fedByOnTime \(date, style: .time) \(name)")
+                                    .font(.title)
+                                    .padding()
+                            }
+                            
+                            Menu {
+                                Button() {
+                                    viewModel.undoFood()
+                                } label: {
+                                    Label("home.areYouSure", systemImage: "questionmark")
+                                        .foregroundColor(.red)
+                                }
+                            } label: {
+                                Text("home.undo")
+                            }
                         }
                         
-                        Menu {
-                            Button() {
-                                viewModel.undoFood()
+                        if !viewModel.hasFood {
+                            Button {
+                                viewModel.giveFood()
                             } label: {
-                                Label("home.areYouSure", systemImage: "questionmark")
-                                    .foregroundColor(.red)
+                                switch viewModel.giveFoodLoading {
+                                case true:
+                                    ProgressView()
+                                case false:
+                                    ButtonLabel(
+                                        title: "home.fedButton",
+                                        imageName: "person.fill.checkmark",
+                                        color: .green
+                                    )
+                                }
                             }
-                        } label: {
-                            Text("home.undo")
+                        }
+                        Spacer()
+                    }
+                }
+                
+                if viewModel.loading {
+                    LoadingView()
+                }
+                
+            }.onAppear(perform: viewModel.load).navigationTitle("home.name")
+                .sheet(isPresented: $viewModel.sheetShown, onDismiss: viewModel.onSheetDismiss){
+                    switch viewModel.whichSheet {
+                    case .historySheet:
+                        FeedingHistory()
+                    case .welcomeSheet:
+                        WelcomeSheet()
+                    case .feedingSheet:
+                        FeedingSheet()
+                    }
+                }
+                .alert(isPresented: $viewModel.alertShown) {
+                    switch viewModel.whichAlert {
+                    case .alreadyFed:
+                        return Alert(
+                            title: Text("alert.alreadyFed.title"),
+                            dismissButton: .cancel(Text("general.ok"))
+                        )
+                    case .connectionError:
+                        return Alert(
+                            title: Text("alert.noConnection.title"),
+                            message: Text("alert.noConnection.message"),
+                            dismissButton: .cancel(Text("general.ok"))
+                        )
+                    }
+                }
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        NavigationLink(destination: SettingsView()) {
+                            Label("settings", systemImage: "gear")
+                                .foregroundColor(.black)
                         }
                     }
-                    
-                    if !viewModel.hasFood {
+                    ToolbarItem(placement: .navigationBarTrailing) {
                         Button {
-                            viewModel.giveFood()
+                            viewModel.load()
                         } label: {
-                            switch viewModel.giveFoodLoading {
-                            case true:
-                                ProgressView()
-                            case false:
-                                ButtonLabel(
-                                    title: "home.fedButton",
-                                    imageName: "person.fill.checkmark",
-                                    color: .green
-                                )
-                            }
+                            Label("home.reload", systemImage: "arrow.clockwise")
+                                .foregroundColor(.black)
                         }
                     }
-                    Spacer()
+                }.onOpenURL { url in
+                    viewModel.onOpenURL(url: url)
                 }
-            }
-            
-            if viewModel.loading {
-                LoadingView()
-            }
-            
-        }.onAppear(perform: viewModel.load).navigationTitle("home.name")
-            .sheet(isPresented: $viewModel.sheetShown, onDismiss: viewModel.onSheetDismiss){
-            switch viewModel.whichSheet {
-            case .historySheet:
-                FeedingHistory()
-            case .welcomeSheet:
-                WelcomeSheet()
-            case .feedingSheet:
-                FeedingSheet()
-            }
-        }
-            .alert(isPresented: $viewModel.alertShown) {
-                switch viewModel.whichAlert {
-                case .alreadyFed:
-                    return Alert(
-                        title: Text("alert.alreadyFed.title"),
-                        dismissButton: .cancel(Text("general.ok"))
-                    )
-                case .connectionError:
-                    return Alert(
-                        title: Text("alert.noConnection.title"),
-                        message: Text("alert.noConnection.message"),
-                        dismissButton: .cancel(Text("general.ok"))
-                    )
-                }
-            }
-            .toolbar {
-            ToolbarItem(placement: .navigationBarTrailing) {
-                NavigationLink(destination: SettingsView()) {
-                    Label("settings", systemImage: "gear")
-                        .foregroundColor(.black)
-                }
-            }
-            ToolbarItem(placement: .navigationBarTrailing) {
-                Button {
-                    viewModel.load()
-                } label: {
-                    Label("home.reload", systemImage: "arrow.clockwise")
-                        .foregroundColor(.black)
-                }
-            }
-            ToolbarItem(placement: .bottomBar) {
-                Button("general.feedingHistory") {
-                    viewModel.showSheet(sheet: .historySheet)
-                }
-            }
-        }.onOpenURL { url in
-            viewModel.onOpenURL(url: url)
         }
     }
 }
