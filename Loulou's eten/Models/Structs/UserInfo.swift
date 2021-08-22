5cdae87befd8dc7fea4e0346fbdbca537cabc0fb
Wisse <wissehes@gmail.com>
2021-08-22 21:13:24 +0200

    Create UserInfo struct

---
+++ Loulou's eten/Models/Structs/UserInfo.swift
@@ -0,0 +1,18 @@
+//
+//  UserInfo.swift
+//  UserInfo
+//
+//  Created by Wisse Hes on 22/08/2021.
+//
+
+import Foundation
+
+struct UserInfo: Codable {
+    let id: Int
+    let name: String
+    let email: String
+    let googleId: String
+    
+    let foodItems: [FoodItem]
+    let pets: [Pet]
+}
