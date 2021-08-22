7a1946a41899c43035e39d332fd61ccb04691657
Wisse <wissehes@gmail.com>
2021-08-22 21:12:13 +0200

    Create CreatePetParams

---
+++ Loulou's eten/Models/Structs/CreatePetParams.swift
@@ -0,0 +1,12 @@
+//
+//  CreatePetParams.swift
+//  CreatePetParams
+//
+//  Created by Wisse Hes on 22/08/2021.
+//
+
+import Foundation
+
+struct CreatePetParams: Codable {
+    let name: String
+}
