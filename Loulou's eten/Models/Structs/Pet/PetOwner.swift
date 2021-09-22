014605342e144189ef7f4b449e34b9f4787a284a
Wisse <wissehes@gmail.com>
2021-09-22 08:08:16 +0200

    Create PetOwner

---
+++ Loulou's eten/Models/Structs/Pet/PetOwner.swift
@@ -0,0 +1,14 @@
+//
+//  PetOwner.swift
+//  PetOwner
+//
+//  Created by Wisse Hes on 04/09/2021.
+//
+
+import Foundation
+
+struct PetOwner: Codable {
+    let id: Int
+    let name: String
+    let email: String
+}
